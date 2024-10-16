bff006688d6c705ef1cfab58e5939cf527c89af3
KYATAP_san_sub <88844260+KYATAPsan@users.noreply.github.com>
2024-10-17 01:49:10 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
         title: "24san",
         logo: {
           alt: "24san Logo",
-          src: "img/logo.svg",
+          src: "https://minotar.net/helm/KYATAP_san_sub/96.png",
         },
         items: [
           {
