11da97003e761c5f8ff2394c9a38b7259213bf17
KYATAP_san_sub <88844260+KYATAPsan@users.noreply.github.com>
2024-10-17 01:37:50 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,28 +11,17 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container flex justify-between items-center"> {/* Flexboxで左右に並べる */}
-        
-        {/* 左側に画像を追加 */}
-        <img 
-          src="Back_KYATAP.png" {/* ここで画像ファイルのパスを指定 */}
-          alt="サーバーイメージ" 
-          className={styles.heroImage} 
-        />
-
-        {/* 右側にタイトルと説明文を表示 */}
-        <div>
-          <Heading as="h1" className="hero__title">
-            {siteConfig.title}
-          </Heading>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/intro">
-              サーバーを見る ➡
-            </Link>
-          </div>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            サーバーを見る ➡
+          </Link>
         </div>
       </div>
     </header>
@@ -43,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="Join the 24san Minecraft Server and experience one of the most dynamic and engaging Minecraft communities!">
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
