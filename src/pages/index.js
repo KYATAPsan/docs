efc9932b7139faa9e4500cc3dec029dad29b444c
KYATAP_san_sub <88844260+KYATAPsan@users.noreply.github.com>
2024-10-19 00:12:13 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,18 +16,20 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/* 画像を追加 */}
-        <img
-          src="https://media.discordapp.net/attachments/1157289480219263056/1296851021083185172/image.png?ex=6713ca21&is=671278a1&hm=ca2edc80ede295579d553e0768f386d131b80f657600879ff36c4970ca3a3161&=&format=webp&quality=lossless&width=807&height=379"
-          alt="Server Image"
-          style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }}
-        />
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
             サーバーを見る ➜
           </Link>
+        </div>
+        {/* 画像をボタンの下に配置 */}
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+          <img
+            src="https://media.discordapp.net/attachments/1157289480219263056/1296851021083185172/image.png?ex=6713ca21&is=671278a1&hm=ca2edc80ede295579d553e0768f386d131b80f657600879ff36c4970ca3a3161&=&format=webp&quality=lossless&width=807&height=379"
+            alt="Server Image"
+            style={{ maxWidth: '100%', height: 'auto' }}
+          />
         </div>
       </div>
     </header>
