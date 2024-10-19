6e94c519fd513a22327fb3f64e0907efd842aaad
KYATAP_san_sub <88844260+KYATAPsan@users.noreply.github.com>
2024-10-19 15:45:57 +0900

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -61,7 +61,7 @@ function Feature({ Svg, title, description }) {
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
-      <div className="text--center padding-horiz--md" style={{ textAlign: 'center' }}>
+      <div className="text--center padding-horiz--md" style={{ textAlign: 'center', maxWidth: '300px' }}>
         <Heading as="h3" className={styles.boldTitle}>
           {title}
         </Heading>
