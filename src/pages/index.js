5989d1f51b68dc62b419fe7ad4b9f2a6e0dea393
KYATAP_san_sub <88844260+KYATAPsan@users.noreply.github.com>
2024-11-23 17:00:37 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,13 +29,6 @@ function HomepageHeader() {
             サーバーの仕様を確認する
           </Link>
         </div>
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-          <img
-            src="https://media.discordapp.net/attachments/1157289480219263056/1296851021083185172/image.png?ex=6713ca21&is=671278a1&hm=ca2edc80ede295579d553e0768f386d131b80f657600879ff36c4970ca3a3161&=&format=webp&quality=lossless&width=807&height=379"
-            alt="Server Image"
-            style={{ maxWidth: '100%', height: 'auto' }}
-          />
-        </div>
       </div>
     </header>
   );
