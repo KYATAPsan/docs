0c4ac63108616ac983bf7c387e833055ba373d5d
KYATAP_san_sub <88844260+KYATAPsan@users.noreply.github.com>
2024-11-23 15:16:11 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -165,7 +165,7 @@ const config = {
       announcementBar: {
         id: "support_us",
         content:
-          '<b><a target="_blank" href="https://discord.gg/TYUnM9vP2E" style="color: #FFFFFF; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\'" onmouseout="this.style.textDecoration=\'none\'">Discordの参加はこちらから！</a></b>',
+          '<b> <a target="_blank" href="https://discord.gg/TYUnM9vP2E" style="color: #FFFFFF; text-decoration: none;" onmouseover="this.style.textDecoration=\'underline\'" onmouseout="this.style.textDecoration=\'none\'">Discordの参加はこちらから！<svg xmlns="http://www.w3.org/2000/svg" style="margin-left: 5px; vertical-align: middle;" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></a></b>',
         backgroundColor: "#161CBB",
         textColor: "#FFFFFF",
         isCloseable: true,
