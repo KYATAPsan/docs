d5d0d2b14c4a1c62743d5e09537d6d15a499c9cc
KYATAP_san_sub <88844260+KYATAPsan@users.noreply.github.com>
2024-11-25 01:43:00 +0900

    Create sidebarsWiki.js

---
+++ sidebarsWiki.js
@@ -0,0 +1,15 @@
+module.exports = {
+  wikiSidebar: [
+    {
+      type: 'category',
+      label: 'Wiki',
+      link: { type: 'doc', id: 'wiki/list' },
+      items: [
+        'wiki/server/index',
+        'wiki/guild/index',
+        'wiki/panel/index',
+        'wiki/api/index',
+      ],
+    },
+  ],
+};
