fef9301c9385f8b83ed7971ac6302ea5642254a9
KYATAP_san_sub <88844260+KYATAPsan@users.noreply.github.com>
2025-05-06 03:54:04 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,11 +20,15 @@ function HomepageHeader() {
         <img src={HeroImage} alt="24san Logo" className={styles.heroImageBackgroundBlur} />
         <div className={styles.textSection}>
           <Heading as="h1" className="hero__title">
-            <Translate id="pages.main.title">Revolutionize Your Minecraft Server</Translate>
+              <strong>
+                <Translate id="pages.main.title">
+                  すべてのプレイヤーがつながる、ひとつの世界
+                </Translate>
+              </strong>
           </Heading>
           <p className="hero__subtitle">
             <Translate id="pages.main.subtitle">
-              Enable clients from Minecraft Bedrock Edition to join your Minecraft Java server
+              Java版も統合版も、24sanサーバーで一緒に遊ぼう
             </Translate>
           </p>
         </div>
